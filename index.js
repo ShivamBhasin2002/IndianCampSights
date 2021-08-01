b694b90245acc949e65191c93a0ed682de216257
Shivam Bhasin <bhasinshivam2002@gmail.com>
2021-08-01 19:39:38 +0530

    Connected to MongoDB database

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require('express')
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/indian-camp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render("index");
+})
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: "NewDelhi" });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
--- models/campground.js
+++ models/campground.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    desc: String,
+    location: String
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
